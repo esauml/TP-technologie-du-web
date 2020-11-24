f6e7e9f5f7704aa936c5ab2edc77f8f5f764742a
unknown <esaumtz4722@gmail.com>
2020-11-24 12:08:45 +0100

    winter lemon done

--- tps/tp2/winter-lemon/src/App.js
+++ tps/tp2/winter-lemon/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import Cad from "./cad";
+import imgNoel from './pere_noel_1.gif';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,12 +11,19 @@ class App extends Component {
   }
   render() {
     return (
+      <>
       <div className="App">
         <h1>Liste de cadeau pour le Père Noel</h1>
         <Cad cad={
-        ["all i want for christmas is you","is you baby",""]
-        }/>
+          ["all i want for christmas is you","is you baby",""]
+          }
+        />
+
+        <a href="https://santaclaus.com/"><button onClick>Aller vers santaclaus.com</button></a>
       </div>
+
+      <img src={imgNoel}></img>
+      </>
     );
   }
 }
