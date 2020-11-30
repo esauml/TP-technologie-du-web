9faf28b3cc519dca8554152e90591c372f52d4ed
Angel <c3po.angel@gmail.com>
2020-11-30 10:18:26 +0100

    mini projet films

---
+++ tps/tp3/movies-lists/build/precache-manifest.2d166f6fe9f92fcbfb846b644a21c756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8094f2f603fabb6e88c63ac60808fd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6635cfd211ebc5892c49",
+    "url": "/static/css/main.b1fb61fb.chunk.css"
+  },
+  {
+    "revision": "74e6eff55737a8ee3bea",
+    "url": "/static/js/2.77920d47.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.77920d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6635cfd211ebc5892c49",
+    "url": "/static/js/main.9818a14c.chunk.js"
+  },
+  {
+    "revision": "b4e86ea48e5fb70ab87b",
+    "url": "/static/js/runtime-main.f3e1d22b.js"
+  }
+]);
---
+++ tps/tp3/movies-lists/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+import films from "./Movies.json";
+
+function Movies(props) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <img src={props.image} alt={props.title} style={{ width: "70%" }} />
+        <h4 className="card__title">{props.fullTitle}</h4>
+          <p>Crew: {props.crew}</p>
+          <p>Year: {props.year}</p>
+          <p>Rating: {props.imDbRating}</p><div className="container">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Movies.propTypes = {
+  image: PropTypes.string,
+  fullTitle: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  crew: PropTypes.string,
+  year: PropTypes.string,
+  imDbRating: PropTypes.string,
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Films</h1>
+      <main className="main-area">
+        <div className="cards">
+          {films.items.map((item, index) => {
+            return <Movies {...item} key={index} />;
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
