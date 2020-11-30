f07467fed4e19bb83807b70e11cca88a7575520b
Angel <c3po.angel@gmail.com>
2020-11-30 15:39:33 +0100

    aaaaah

--- tps/tp4/timer-hamster/src/App.js
+++ tps/tp4/timer-hamster/src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react'
+
+function Minutos(){
+
+}
+function Segundos({sec,setSec}){
+let a=setInterval(()=>{
+ if(sec<60){
+   setSec(parseInt(sec)+1)
+ }
+ else{
+   setSec(0)
+ }
+},1000)
+return (<p>{sec}</p> );
+}
 function App() {
  const [min,setMin]=React.useState(0);
  const [sec,setSec]=React.useState(0);
  const [flag,setFlag]=React.useState(false)
- let a=!flag?setInterval(() => {
-   if(sec==60){
+/*  let a=!flag?setInterval(() => {
+   if(sec===59){
      setMin(min+1)
     setSec(0);
    }
    else{
   setSec(sec+1);}
- },1000 ):null;
+ },1000 ):null; */
+const handleClick=e=>{
+  e.preventDefault()
+  setFlag(!flag);
+  setMin(0);
+  setSec(0);
+}
   return (
     <div>
-      <p>{min}:{sec}</p>
-      <button onClick={()=>setFlag(true)}>Arrete</button>
+      <Segundos sec={parseInt(sec)} setSec={setSec}/>
+      <button onClick={handleClick}>Arrete</button>
     </div>
   );
 }
