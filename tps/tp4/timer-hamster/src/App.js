38fb79cd59b944b8554e2a44025692d3637021cf
Angel <c3po.angel@gmail.com>
2020-11-30 17:50:32 +0100

    ne marche pas

--- tps/tp4/timer-hamster/src/App.js
+++ tps/tp4/timer-hamster/src/App.js
@@ -1,43 +1,70 @@
+import "./App.css";
+import React from "react";
+import SevenSegmentDisplay from "./react-seven-segment-display";
 
-import './App.css';
-import React from 'react'
-
-function Minutos(){
-
+function Minutos({ min, setMin }) {
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (min < 99) {
+        setMin(min=>min+1);
+      } else {
+        setMin(0);
+      }
+    }, 60000);
+    return () => clearInterval(interval);
+  },[]);
+  return min > 9 ? (
+    <>
+      <SevenSegmentDisplay value={min.toString(10).split("")[0]} />
+      <SevenSegmentDisplay value={min.toString(10).split("")[1]} />
+    </>
+  ) : (
+    <>
+      <SevenSegmentDisplay value="0" />
+      <SevenSegmentDisplay value={min} />
+    </>
+  );
 }
-function Segundos({sec,setSec}){
-let a=setInterval(()=>{
- if(sec<60){
-   setSec(parseInt(sec)+1)
- }
- else{
-   setSec(0)
- }
-},1000)
-return (<p>{sec}</p> );
+function Segundos({ sec, setSec }) {
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (sec < 60) {
+        setSec(1 + sec);
+      } else {
+        setSec(0);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  return sec > 9 ? (
+    <>
+      <SevenSegmentDisplay value={sec.toString(10).split("")[0]} />
+      <SevenSegmentDisplay value={sec.toString(10).split("")[1]} />
+    </>
+  ) : (
+    <>
+      <SevenSegmentDisplay value="0" />
+      <SevenSegmentDisplay value={sec} />
+    </>
+  );
 }
 function App() {
- const [min,setMin]=React.useState(0);
- const [sec,setSec]=React.useState(0);
- const [flag,setFlag]=React.useState(false)
-/*  let a=!flag?setInterval(() => {
-   if(sec===59){
-     setMin(min+1)
+  const [min, setMin] = React.useState(0);
+  const [sec, setSec] = React.useState(0);
+  const [flag, setFlag] = React.useState(false);
+  const [pausa, setPause]=React.useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setFlag(!flag);
+    setMin(0);
     setSec(0);
-   }
-   else{
-  setSec(sec+1);}
- },1000 ):null; */
-const handleClick=e=>{
-  e.preventDefault()
-  setFlag(!flag);
-  setMin(0);
-  setSec(0);
-}
+  };
   return (
     <div>
-      <Segundos sec={parseInt(sec)} setSec={setSec}/>
-      <button onClick={handleClick}>Arrete</button>
+      <Minutos min={min} setMin={setMin} />:
+      <Segundos sec={sec} setSec={setSec} />
+      <button onClick={handleClick}>reboot</button>
     </div>
   );
 }
